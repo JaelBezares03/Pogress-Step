75afed4eb381e417a6a6d48764e2480903ee2a81
Jael <bezaresjael@gmail.com>
2024-02-09 21:35:17 -0600

    last commit

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
-const circles = document.querySelectorAll('circle');
-const linea = document.getElementsByClassName('linea-horizontal');
-let indexSteps = 0;
+const circles = document.querySelectorAll('.circle');
+const btnNext = document.getElementById('next');
+const btnFormer = document.getElementById('former');
+const lines = document.querySelectorAll('.linea-horizontal');
+let indexStep = 0;
+
+circles[0].classList.add('activo');
+btnNext.addEventListener('click', function(){
+    if(indexStep < circles.length - 1){ // Verifica si indexStep está dentro del rango válido
+        lines[indexStep].classList.add('activo');
+        circles[indexStep+1].classList.add('activo'); // Agrega la clase activo al círculo correspondiente
+        indexStep++;
+    }
+    }
+)
+
+btnFormer.addEventListener('click', function(){
+    if(indexStep>0){
+    lines[indexStep-1].classList.remove('activo');
+    circles[indexStep].classList.remove('activo');
+    indexStep--;
+}})
+
